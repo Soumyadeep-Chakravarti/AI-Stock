08a0d7504732123d772fd0a6b77626f3ec924f22
Soumyadeep Chakravarti <86187991+Soumyadeep-Chakravarti@users.noreply.github.com>
2024-04-29 21:57:47 +0530

    c

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #main
 
 import os
-from scripts import data_formating as data_format
-from scripts import file_downloader as fd
-from scripts import web_copier as wc
-from scripts import imp_items
+import data_formating as data_format
+import file_downloader as fd
+import web_copier as wc
+import imp_items
 
 downloader = fd.FileDownloader()
 WEBSITE_URL = imp_items.website_urls[1]
