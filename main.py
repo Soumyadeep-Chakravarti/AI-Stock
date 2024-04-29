e56fb8afca75535267865958e9e4957e3ed84a6b
Soumyadeep Chakravarti <86187991+Soumyadeep-Chakravarti@users.noreply.github.com>
2024-04-29 21:11:47 +0530

    added new mechanisms

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import os
+#main
 
-import file_downloader as fd
-import web_copier as wc
+import os
+from scripts import data_formating as data_format
+from scripts import file_downloader as fd
+from scripts import web_copier as wc
 import imp_items
 
 downloader = fd.FileDownloader()
@@ -9,4 +11,6 @@ WEBSITE_URL = imp_items.website_urls[1]
 OUTPUT_DIR = os.path.join(imp_items.paths[1],imp_items.formatted_today_date)
 web_copier = wc.WebTableCopier()
 web_copier.save_tables_from_url(WEBSITE_URL, OUTPUT_DIR)
-downloader.download_and_extract(imp_items.website_urls[0],OUTPUT_DIR)
+path1 = downloader.download_and_extract(imp_items.website_urls[0],OUTPUT_DIR)
+processor = data_format.CompanyDataProcessor(OUTPUT_DIR,os.path.join(OUTPUT_DIR,'table.csv'))
+processor.merge_and_save()
