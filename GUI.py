64297f3947b30281411c899cc89dcd2feaf3aa39
Soumyadeep Chakravarti <86187991+Soumyadeep-Chakravarti@users.noreply.github.com>
2024-05-13 02:24:08 +0530

    changes

--- GUI.py
+++ GUI.py
@@ -1,7 +1,36 @@
+"""
+This script provides a graphical user interface (GUI) for the stock analysis tool using Tkinter.
+
+Classes:
+    StockAnalysisGUI: A class to create the GUI for the stock analysis tool.
+
+Usage:
+    1. Ensure you have the necessary CSV files containing historical stock market data.
+    2. Replace the placeholder file paths in the `stock_data_paths` list with the actual file paths.
+    3. Run the script. The GUI window will appear.
+    4. Select a company from the dropdown menu.
+    5. Click the "Analyze" button to perform analysis and display trade information.
+
+Attributes:
+    master (Tk): The Tkinter root window.
+
+Methods:
+    __init__(self, master): Initializes the GUI with labels, dropdown menu, and analyze button.
+    analyze_company(self): Performs analysis on the selected company and displays trade information.
+
+Dependencies:
+    - tkinter: Used to create the GUI.
+    - ttk: Used for themed widgets in the GUI.
+    - messagebox: Used to display message boxes in the GUI.
+    - StockAnalysis: Imported from the stock_analysis module to perform analysis on stock market data.
+"""
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
-from stock_analysis import StockAnalysis  # Import the StockAnalysis class from your analysis module
+import imp_items
+from stock_analysis import StockAnalysis
+  # Import the StockAnalysis class from your analysis module
 
 class StockAnalysisGUI:
     def __init__(self, master):
@@ -17,7 +46,7 @@ class StockAnalysisGUI:
         self.analyze_button = ttk.Button(master, text="Analyze", command=self.analyze_company)
         self.analyze_button.grid(row=1, column=0, columnspan=2, padx=10, pady=5)
 
-        self.stock_data_paths = ["company1.csv", "company2.csv"]  # Replace with actual file paths
+        self.stock_data_paths = imp_items.find_csv_files(imp_items.paths[2])# Replace with actual file paths
         self.combo['values'] = [f"Company {i+1}" for i in range(len(self.stock_data_paths))]
 
     def analyze_company(self):
--- imp_items.py
+++ imp_items.py
@@ -28,6 +28,23 @@ def format_date(input_date):
 
     return formatted_date
 
+def find_csv_files(folder):
+    """
+    Find CSV files in a folder and its subdirectories.
+
+    Args:
+    folder (str): Path to the folder to search.
+
+    Returns:
+    list: A list of paths to CSV files found.
+    """
+    csv_files = []
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith(".csv"):
+                csv_files.append(os.path.join(root, file))
+    return csv_files
+
 today_date = datetime.today()
 yesterday_date = today_date - timedelta(days = 1)
 # Format the date using the format_date function
